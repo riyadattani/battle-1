f3dc6fbcdd635986444a105aab2984ac31ade44f
Riya Dattani <riya_dattani@hotmail.com>
2019-03-22 17:42:38 +0000

    tyding up

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,11 @@
 class Player
   attr_reader :name, :hit_points
   DEFAULT_HP = 50
+  
   def initialize(name, hit_points = DEFAULT_HP)
     @name = name
     @hit_points = hit_points
   end
-
-  # def attack(name)
-  #   name.damage
-  # end
 
   def damage
     @hit_points -=10
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,13 +12,6 @@ describe Player do
     expect(arthur.hit_points).to eq described_class::DEFAULT_HP
   end
 
-  # describe "#attack" do
-  #   it "a player will be attacked and gets damaged" do
-  #     expect(riya).to receive(:damage)
-  #     arthur.attack(riya)
-  #   end
-  # end
-
   describe "#damage" do
     it "should reduce HP by 10 when attack clicked" do
       expect { riya.damage }.to change { riya.hit_points }.by(-10)
